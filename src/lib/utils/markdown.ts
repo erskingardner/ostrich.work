51f293f3e5afa1b90a41aefe95e1d92dd31433b0
Jeff Gardner <202880+erskingardner@users.noreply.github.com>
2023-07-13 17:24:26 +0200

    Fix dompurify

--- src/lib/utils/markdown.ts
+++ src/lib/utils/markdown.ts
@@ -1,13 +1,13 @@
 import { marked } from 'marked';
-import { sanitize } from 'isomorphic-dompurify';
 import { gfmHeadingId } from 'marked-gfm-heading-id';
 import { mangle } from 'marked-mangle';
+import DOMPurify from 'isomorphic-dompurify';
 
 export function cleanMarkdown(content: string): string {
     marked.use(mangle());
     marked.use(gfmHeadingId());
 
-    return sanitize(
+    return DOMPurify.sanitize(
         // eslint-disable-next-line no-misleading-character-class
         marked.parse(content.replace(/^[\u200B\u200C\u200D\u200E\u200F\uFEFF]/, ''))
     );
