1ba3cb49b78ee49a260b1d70a09262b1f48fda02
Jeff Gardner <202880+erskingardner@users.noreply.github.com>
2023-07-13 17:17:21 +0200

    Fix loading of job page on refresh

--- src/routes/jobs/[job]/+page.svelte
+++ src/routes/jobs/[job]/+page.svelte
@@ -1,34 +1,46 @@
 <script lang="ts">
-    import type { NDKEvent } from "@nostr-dev-kit/ndk";
     import { displayableName, firstTagValue, formattedDate } from "$lib/utils/helpers.js";
     import ndk from "$lib/stores/ndk.js";
-    import type { NDKUser, NDKTag } from "@nostr-dev-kit/ndk";
+    import type { NDKEvent, NDKUser, NDKTag } from "@nostr-dev-kit/ndk";
     import { Avatar } from "@nostr-dev-kit/ndk-svelte-components";
     import { cleanMarkdown } from "$lib/utils/markdown.js";
     import JobStatPills from "$lib/components/JobStatPills.svelte";
     import { contractTypeOptions, categoryOptions } from "$lib/data/formOptions.js";
 
     export let data;
-    const job: NDKEvent = data.job as NDKEvent;
 
-    let title:string = firstTagValue(job, "title");
-    let description:string = job.content;
-    let location:string = firstTagValue(job, "location");
-    let summary:string = firstTagValue(job, "summary");
+    let job: NDKEvent;
+    let title:string;
+    let description:string;
+    let location:string;
+    let summary:string;
     let contractType:string;
     let jobCategories:string[] = [];
-    let price:string | undefined = firstTagValue(job, "price");
-    let publishedAt:number = parseInt(firstTagValue(job, "published_at")) * 1000;
-    let author: NDKUser = $ndk.getUser({hexpubkey: job.pubkey});
+    let price:string | undefined;
+    let publishedAt:number;
+    let author: NDKUser;
+    let hashtags: NDKTag[]
 
-    let hashtags: NDKTag[] = job.getMatchingTags("t");
-    hashtags.forEach((tag) => {
-        let contractTypeMatch = contractTypeOptions.find(element => element.value === tag[1]);
-        if (contractTypeMatch) contractType = contractTypeMatch.name;
 
-        let categoryMatch = categoryOptions.find(element => element.value === tag[1]);
-        if (categoryMatch) jobCategories.push(categoryMatch.name);
+    $ndk.fetchEvent(data.jobAddr).then((event) => {
+        job = event as NDKEvent;
+        title = firstTagValue(job, "title");
+        description = job.content;
+        location = firstTagValue(job, "location");
+        summary = firstTagValue(job, "summary");
+        price = firstTagValue(job, "price");
+        publishedAt = parseInt(firstTagValue(job, "published_at")) * 1000;
+        author = $ndk.getUser({hexpubkey: job.pubkey});
+        hashtags = job.getMatchingTags("t");
+        hashtags.forEach((tag) => {
+            let contractTypeMatch = contractTypeOptions.find(element => element.value === tag[1]);
+            if (contractTypeMatch) contractType = contractTypeMatch.name;
+
+            let categoryMatch = categoryOptions.find(element => element.value === tag[1]);
+            if (categoryMatch) jobCategories.push(categoryMatch.name);
+        });
     });
+
 </script>
 
 <svelte:head>
@@ -39,22 +51,26 @@
     />
 </svelte:head>
 
-<div class="prose dark:prose-invert mx-auto">
-    <h1 class="mt-10 mb-2 text-orange-600 border-b-2 border-b-orange-500 pb-1">{title}</h1>
-    <span class="italic text-lg font-bold">{summary}</span>
-    {#await author.fetchProfile() then value}
-        <div class="flex flex-row gap-4 items-center font-medium mt-10">
-            <a href={`/${author.npub}`} class="border-none"><Avatar ndk={$ndk} npub={author.npub} class="w-10 h-10 m-0 shadow-square-grey-sm hover:shadow-square-orange hover:duration-500 duration-1000" /></a>
-            <span class="mt-2">Posted by {displayableName(author)} on {formattedDate(publishedAt)}</span>
+{#key job}
+    {#if job}
+        <div class="prose dark:prose-invert mx-auto">
+            <h1 class="mt-10 mb-2 text-orange-600 border-b-2 border-b-orange-500 pb-1">{title}</h1>
+            <span class="italic text-lg font-bold">{summary}</span>
+            {#await author.fetchProfile() then value}
+                <div class="flex flex-row gap-4 items-center font-medium mt-10">
+                    <a href={`/${author.npub}`} class="border-none"><Avatar ndk={$ndk} npub={author.npub} class="w-10 h-10 m-0 shadow-square-grey-sm hover:shadow-square-orange hover:duration-500 duration-1000" /></a>
+                    <span class="mt-2">Posted by {displayableName(author)} on {formattedDate(publishedAt)}</span>
+                </div>
+            {/await}
+            <div class="mt-4">
+                <JobStatPills {location} {price} {contractType} {jobCategories} />
+            </div>
+            <div class="flex flex-col gap-8">
+                <div>
+                    <h2 class="text-orange-600 border-b-2 border-b-orange-500">Job Description</h2>
+                    <div class="markdownContent">{@html cleanMarkdown(description)}</div>
+                </div>
+            </div>
         </div>
-    {/await}
-    <div class="mt-4">
-        <JobStatPills {location} {price} {contractType} {jobCategories} />
-    </div>
-    <div class="flex flex-col gap-8">
-        <div>
-            <h2 class="text-orange-600 border-b-2 border-b-orange-500">Job Description</h2>
-            <div class="markdownContent">{@html cleanMarkdown(description)}</div>
-        </div>
-    </div>
-</div>
+    {/if}
+{/key}
--- src/routes/jobs/[job]/+page.ts
+++ src/routes/jobs/[job]/+page.ts
@@ -1,13 +1,9 @@
 /** @type {import('./$types').PageLoad} */
-import { get as getStore } from 'svelte/store';
-import ndkStore from '$lib/stores/ndk';
 
-export function load({ params }) {
+export async function load({ params }) {
     const jobAddr = params.job;
-    const ndk = getStore(ndkStore);
-    const job = ndk.fetchEvent(jobAddr);
 
     return {
-        job: job
+        jobAddr
     };
 }
