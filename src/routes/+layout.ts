7b55bf6af6711e6893c82137894541dc16e868f7
Jeff Gardner <202880+erskingardner@users.noreply.github.com>
2023-07-14 16:30:55 +0200

    Downgrade ndk to 0.7.5

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -3,9 +3,9 @@ import { get } from 'svelte/store';
 
 export async function load() {
     const ndk = get(ndkStore);
-    ndk.connect().catch((e) => {
-        console.error(e);
-    });
+    ndk.connect()
+        // .then(() => console.log('Connected'))
+        .catch((e) => console.error(e));
 
     return {};
 }
