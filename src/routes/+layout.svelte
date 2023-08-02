b922285ce10871cb434a327a9a680df513f490d1
Jeff Gardner <202880+erskingardner@users.noreply.github.com>
2023-08-02 08:44:02 +0200

    Add a modal when we don't have a nip-07 signer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,15 +9,17 @@
     import { goto } from '$app/navigation';
     import { browser } from '$app/environment';
     import toast, { Toaster } from 'svelte-french-toast';
+    import { Button, Modal } from 'flowbite-svelte';
 
     let savestore = false;
+    let signerModal = false;
 
     $: if (savestore && $currentUser) {
         // Get the user
         window.sessionStorage.setItem('ostrichWorkCurrentUser', JSON.stringify($currentUser));
     }
 
-    if (browser)   {
+    if (browser) {
         const storedUser = window.sessionStorage.getItem('ostrichWorkCurrentUser');
         if (storedUser) {
             currentUser.set(JSON.parse(storedUser));
@@ -29,21 +31,28 @@
     }
 
     async function login(domEvent: any) {
-        const signer = new NDKNip07Signer();
-        $ndk.signer = signer;
-        ndk.set($ndk);
-        signer.user().then(async (ndkUser) => {
-            if (!!ndkUser.npub) {
-                ndkUser.ndk = $ndk;
-                currentUser.set(ndkUser);
-                window.sessionStorage.setItem('ostrichWorkCurrentUser', JSON.stringify(ndkUser));
-                document.cookie = `userNpub=${ndkUser.npub};
+        try {
+            const signer = new NDKNip07Signer();
+            $ndk.signer = signer;
+            ndk.set($ndk);
+            signer.user().then(async (ndkUser) => {
+                if (!!ndkUser.npub) {
+                    ndkUser.ndk = $ndk;
+                    currentUser.set(ndkUser);
+                    window.sessionStorage.setItem(
+                        'ostrichWorkCurrentUser',
+                        JSON.stringify(ndkUser)
+                    );
+                    document.cookie = `userNpub=${ndkUser.npub};
                 expires=${dateTomorrow()}; SameSite=Lax; Secure`;
-                toast.success("Logged in");
-            }
-        });
-
-        if (domEvent?.detail?.redirect) goto(domEvent.detail.redirect);
+                    toast.success('Logged in');
+                }
+            });
+            if (domEvent?.detail?.redirect) goto(domEvent.detail.redirect);
+        } catch (error: any) {
+            console.error(error.message);
+            signerModal = true;
+        }
     }
 
     function logout(e: Event) {
@@ -56,6 +65,44 @@
 </script>
 
 <Toaster />
+
+<Modal title="Nostr Signing Extension Not Found" bind:open={signerModal} autoclose>
+    <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+        Ostrich Work is an app built on <span class="font-bold">Nostr</span>. To log in and use the
+        site you need to sign events with your Nostr keys using a browser extension like Alby or
+        nos2x in Chrome or Brave.
+    </p>
+    <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+        If you're unsure what Nostr is and want to learn more please check out <a
+            href="https://nostr.how?utm_source=ostrichWork&utm_medium=signerModal">Nostr.how</a
+        >
+    </p>
+    <svelte:fragment slot="footer">
+        <div
+            class="flex flex-col gap-8 md:flex-row items-center justify-center md:justify-around w-full"
+        >
+            <a
+                href="https://getalby.com?utm_source=ostrichWork&utm_medium=signerModal"
+                class="not-styled mx-auto flex flex-row py-2 px-4 focus:outline-none border-none no-underline text-lg transition-all duration-1000 hover:duration-500 font-extrabold italic text-white bg-purple-700 -skew-x-12 shadow-square-grey hover:shadow-square-orange-lg disabled:hover:shadow-square-grey disabled:opacity-70"
+            >
+                <span class="skew-x-12">Get Alby</span>
+            </a>
+            <a
+                href="https://github.com/fiatjaf/nos2x"
+                class="not-styled mx-auto flex flex-row py-2 px-4 focus:outline-none border-none no-underline text-lg transition-all duration-1000 hover:duration-500 font-extrabold italic text-white bg-purple-700 -skew-x-12 shadow-square-grey hover:shadow-square-orange-lg disabled:hover:shadow-square-grey disabled:opacity-70"
+            >
+                <span class="skew-x-12">Get nos2X</span>
+            </a>
+            <a
+                href="https://nostr.how?utm_source=ostrichWork&utm_medium=signerModal"
+                class="not-styled mx-auto flex flex-row py-2 px-4 focus:outline-none border-none no-underline text-lg transition-all duration-1000 hover:duration-500 font-extrabold italic text-white bg-purple-700 -skew-x-12 shadow-square-grey hover:shadow-square-orange-lg disabled:hover:shadow-square-grey disabled:opacity-70"
+            >
+                <span class="skew-x-12">Learn more</span>
+            </a>
+        </div>
+    </svelte:fragment>
+</Modal>
+
 <div class="mx-auto md:max-w-5xl lg:max-w-7xl px-4">
     <Header on:login={login} on:logout={logout} />
     <div class="md:max-w-5xl lg:max-w-7xl mx-auto prose dark:prose-invert">
